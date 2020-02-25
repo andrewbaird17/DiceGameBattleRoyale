18002e3a466dabb04739de9136c50027c074f38a
Andrew Baird <baird.andrew4@gmail.com>
2020-02-25 10:40:47 -0600

    Base functions laid out in js file

--- dicegame.js
+++ dicegame.js
@@ -1,5 +1,22 @@
 "use strict";
 
 function runGame(){
-    
+
 }
+
+function startRound(){
+
+}
+
+function rollDiceSet(){
+
+}
+
+function addDice(d4,d6,d8,d10,d12,d20){
+
+}
+
+function shootoutRoll(){
+
+}
+
