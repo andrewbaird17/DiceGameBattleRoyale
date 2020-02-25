84ef9c753ffafca60b957d41af03d193c0ad842c
Andrew Baird <baird.andrew4@gmail.com>
2020-02-25 13:40:56 -0600

    Added two more functions that will be used in the startRound function where the game logic happens

--- dicegame.js
+++ dicegame.js
@@ -4,22 +4,46 @@ var shootoutRolls = [rollOne, rollTwo, rollThree, rollFour]
 var player = {totalDice: 0,shootoutRolls}
 var players = [player, player, player, player, player, player, player, player, player, player]
 var roundNumber = 1;
-function runGame(){
-    if (players.length > 6){
-        // loop twice and find the player with the lowest totalDice and remove them each time
 
+function startRound(){
+    if (players.length >= 6){
+        // loop twice and find the player with the lowest totalDice and remove them each time
+        for (player in players){
+            player.totalDice = rollDiceSet();
+        }
+        let indexNum = compareResults();
+        removePlayer(indexNum);
+        let indexNum2 = compareResults();
+        removePlayer(indexNum2);
+        round++
     } else if(players.length > 2){
         // remove the player with the lowest totalDice and remove them
-
+        for (player in players){
+            player.totalDice = rollDiceSet();
+        }
+        let indexNum = compareResults();
+        removePlayer(indexNum);
+        round++;
     } else{
+        for (player in players){
+            player.totalDice = player.shootoutRoll();
+        }
 
     }
 }
 
-function startRound(){
-    players.forEach(rollDiceSet)
+function compareResults(){
+    for(i=0; i < players.length - 1;i++){
+        if (i !== players.length - 1){
+            
+        }
+    }
+
 }
 
+function removePlayer(indexNum){
+    players.splice(indexNum,1);
+}
 function rollDiceSet(){
     let d4 = getRndNum(1,4);
     let d6 = getRndNum(1,6);
@@ -39,13 +63,16 @@ function addDice(d4,d6,d8,d10,d12,d20){
 
 function shootoutRoll(){
     let rollOne = getRndNum(1,20);
-    player.shootoutRolls[0] = rollOne;
+    shootoutRolls[0] = rollOne;
     let rollTwo = getRndNum(1,20);
     shootoutRolls[1] = rollTwo;
     let rollThree = getRndNum(1,20);
     shootoutRolls[2] = rollThree;
     let rollFour = getRndNum(1,20);
     shootoutRolls[3] = rollFour;
+    let num = getRndNum(0,3);
+    let totalDice = shootoutRolls[num];
+    return totalDice;
 }
 
 function getRndNum(min,max){
