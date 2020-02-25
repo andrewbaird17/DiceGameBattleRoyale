63e215cf6a3ec4916a787a5b9babb962f1e6ae6b
Andrew Baird <baird.andrew4@gmail.com>
2020-02-25 10:52:38 -0600

    Added a get random number function to use in the rolldice function

--- dicegame.js
+++ dicegame.js
@@ -9,6 +9,13 @@ function startRound(){
 }
 
 function rollDiceSet(){
+    let d4 = getRndNum(1,4);
+    let d6 = getRndNum(1,6);
+    let d8 = getRndNum(1,8);
+    let d10 = getRndNum(1,10);
+    let d12 = getRndNum(1,12);
+    let d20 = getRndNum(1,20);
+
 
 }
 
@@ -20,3 +27,6 @@ function shootoutRoll(){
 
 }
 
+function getRndNum(min,max){
+    return Math.floor(Math.random()* (max - min + 1)) + min;
+}
