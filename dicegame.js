e8918978953132359b6c93d35a2d0099bbdc84eb
Andrew Baird <baird.andrew4@gmail.com>
2020-02-25 11:09:22 -0600

    Working on the logic of what will happen in a certain round. Something different will happen for 1-3, 4-5, 6. Created a base array to store the players.

--- dicegame.js
+++ dicegame.js
@@ -1,7 +1,18 @@
 "use strict";
 
+const players = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10]
 function runGame(){
+    for(let i = 1; i <= 6; i++){
+        if (i <= 3){
 
+        }
+        else if(i==4 || i==5){
+
+        }
+        else{
+
+        }
+    }
 }
 
 function startRound(){
@@ -15,8 +26,8 @@ function rollDiceSet(){
     let d10 = getRndNum(1,10);
     let d12 = getRndNum(1,12);
     let d20 = getRndNum(1,20);
-
-
+    let totalDice = addDice(d4,d6,d8,d10,d12,d20);
+    return totalDice;
 }
 
 function addDice(d4,d6,d8,d10,d12,d20){
